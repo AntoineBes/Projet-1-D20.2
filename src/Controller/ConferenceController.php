923b16e38f9f48c09a9cddb652535e760f3027a3
AntoineMT <antoinemt@outlook.fr>
2019-02-13 12:22:34 +0100

    Gestion des conferences add/edit/remove

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Conference;
+use App\Form\ConferenceType;
+use App\Repository\ConferenceRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
+class ConferenceController extends AbstractController
+{
+    /**
+     * @Route("/conference", name="conference")
+     */
+    public function index(ConferenceRepository $conferenceRepository)
+    {
+        
+        $infosConf = $conferenceRepository->findAll();
+        return $this->render('conference/index.html.twig', [
+            'controller_name' => 'ConferenceController',
+            'allConf' => $infosConf,
+        ]);
+    }
+    
+    /**
+    * @Route("/ajout-conference", name="ajout conference")
+    */
+    public function addConference(Request $request)
+    {
+            $conf = new Conference();
+            $form = $this->createForm(ConferenceType::class, $conf);
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($conf);
+                $entityManager->flush();
+                $this->addFlash('notice', 'Conference crée');
+                return $this->redirectToRoute('conference'); 
+            } 
+        return $this->render('conference/ajoutConf.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+    
+    
+     /**
+    * @Route("/conf-edit/{conference_id}", requirements={"id"=".+"}, name="conf_modif")
+    * @ParamConverter("conference", options={"id" = "conference_id"})
+    */
+    
+     public function confModif( ConferenceRepository $ConferenceRepository,Conference $conference,Request $request)
+    {
+            $conf = $conference;
+            $form = $this->createForm(ConferenceType::class, $conf);
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($conf);
+                $entityManager->flush();
+                $this->addFlash('notice', 'Conference modifier');
+                return $this->redirectToRoute('conference'); 
+            } 
+     
+        return $this->render('conference/modifConf.html.twig', [
+            'form' => $form->createView(),
+        ]);
+         
+    }
+    
+     /**
+    * @Route("/conf-remove/{conference_id}", requirements={"id"=".+"}, name="conf_remove")
+    * @ParamConverter("conference", options={"id" = "conference_id"})
+    */
+    
+     public function confRemove( ConferenceRepository $ConferenceRepository,Conference $conference,Request $request)
+    {
+                $em = $this->getDoctrine()->getEntityManager();
+                $em->remove($conference);
+                $em->flush();
+                $this->addFlash('notice', 'Conference supprimer');
+                return $this->redirectToRoute('conference');
+    }
+    
+ 
+    
+    
+    
+}
---
+++ src/Entity/Conference.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ConferenceRepository")
+ */
+class Conference
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adresse;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $intervenant;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $sousTitre;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getIntervenant(): ?string
+    {
+        return $this->intervenant;
+    }
+
+    public function setIntervenant(?string $intervenant): self
+    {
+        $this->intervenant = $intervenant;
+
+        return $this;
+    }
+
+    public function getSousTitre(): ?string
+    {
+        return $this->sousTitre;
+    }
+
+    public function setSousTitre(?string $sousTitre): self
+    {
+        $this->sousTitre = $sousTitre;
+
+        return $this;
+    }
+}
