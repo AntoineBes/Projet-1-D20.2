7cc1fc50801b81d351c4bc6c003c5f47ff252897
lucasfoussier <lucasfoussier@gmail.com>
2019-02-13 17:39:02 +0100

    ajout du date time picker et mise en forme des formualaires

--- src/Form/ConferenceType.php
+++ src/Form/ConferenceType.php
@@ -6,27 +6,34 @@ use App\Entity\Conference;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class ConferenceType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+class ConferenceType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('adresse')
-            ->add('date')
-            ->add('intervenant')
-            ->add('sous_titre')
-            ->add('submit', SubmitType::class)
+                ->add('title')
+                ->add('description')
+                ->add('adresse')
+                ->add('date', DateTimeType::class, [
+                    'widget' => 'single_text',
+                    
+                    'html5' => false,
+                    
+                    'attr' => ['class' => 'js-datepicker'],
+                ])
+                ->add('intervenant')
+                ->add('sous_titre')
+                ->add('submit', SubmitType::class)
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver)
-    {
+    public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults([
             'data_class' => Conference::class,
         ]);
     }
+
 }
