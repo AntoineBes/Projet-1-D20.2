dbf2b2313dac9c88d5646faa3df4c84e71265e40
AntoineMT <antoinemt@outlook.fr>
2019-02-13 16:43:02 +0100

    ajout gestion-mails

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Conference;
 use App\Form\ConferenceType;
 use App\Repository\ConferenceRepository;
+use App\Repository\UserRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class ConferenceController extends AbstractController
@@ -28,7 +29,7 @@ class ConferenceController extends AbstractController
     /**
     * @Route("/admin/ajout-conference", name="ajout conference")
     */
-    public function addConference(Request $request)
+    public function addConference(Request $request, MailController $mailController,UserRepository $userRepository)
     {
             $conf = new Conference();
             $form = $this->createForm(ConferenceType::class, $conf);
@@ -37,6 +38,10 @@ class ConferenceController extends AbstractController
                 $entityManager = $this->getDoctrine()->getManager();
                 $entityManager->persist($conf);
                 $entityManager->flush();
+                $users = $userRepository->findAll();
+                foreach ($users as $user){
+                    $mailController->index($user);
+                }
                 $this->addFlash('notice', 'Conference crée');
                 return $this->redirectToRoute('conference'); 
             } 
@@ -75,7 +80,7 @@ class ConferenceController extends AbstractController
     * @ParamConverter("conference", options={"id" = "conference_id"})
     */
     
-     public function confRemove( ConferenceRepository $ConferenceRepository,Conference $conference,Request $request)
+     public function confRemove(ConferenceRepository $ConferenceRepository,Conference $conference,Request $request)
     {
                 $em = $this->getDoctrine()->getEntityManager();
                 $em->remove($conference);
--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -4,16 +4,41 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
 
-class MailController extends AbstractController
-{
+class MailController extends AbstractController {
+
+    private $mailler;
+    
+    public function __construct(\Swift_Mailer $mailer) {
+        $this->mailler = $mailer;
+    }
+
     /**
      * @Route("/mail", name="mail")
      */
-    public function index()
-    {
-        return $this->render('mail/index.html.twig', [
-            'controller_name' => 'MailController',
-        ]);
+    public function index(User $user) {
+        $message = (new \Swift_Message('Nouvelle conférence en ligne a ne louper sous aucun pretexte !'))
+                ->setFrom('conf2000@msn.com')
+                ->setTo($user->getEmail())
+                ->setBody(
+                $this->renderView(
+                        'mail/registration.html.twig', ['user' => $user]
+                ), 'text/html'
+                )
+        /*
+         * If you also want to include a plaintext version of the message
+          ->addPart(
+          $this->renderView(
+          'emails/registration.txt.twig',
+          ['name' => $name]
+          ),
+          'text/plain'
+          )
+         */
+        ;
+
+        $this->mailler->send($message);
     }
+
 }
