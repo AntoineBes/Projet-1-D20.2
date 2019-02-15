c2a8ebd12ddd495d03bd446ce2ea3b86c899b131
Sacar <antoine95560@hotmail.fr>
2019-02-15 11:16:30 +0100

    created entity Vote

--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -46,6 +48,17 @@ class Conference
      */
     private $sousTitre;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Vote", mappedBy="conf_id")
+     */
+    private $conf_vote;
+
+    public function __construct()
+    {
+        $this->conf_vote = new ArrayCollection();
+    }
+
+   
     public function getId(): ?int
     {
         return $this->id;
@@ -122,4 +135,37 @@ class Conference
 
         return $this;
     }
+
+    /**
+     * @return Collection|Vote[]
+     */
+    public function getConfVote(): Collection
+    {
+        return $this->conf_vote;
+    }
+
+    public function addConfVote(Vote $confVote): self
+    {
+        if (!$this->conf_vote->contains($confVote)) {
+            $this->conf_vote[] = $confVote;
+            $confVote->setConfId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeConfVote(Vote $confVote): self
+    {
+        if ($this->conf_vote->contains($confVote)) {
+            $this->conf_vote->removeElement($confVote);
+            // set the owning side to null (unless already changed)
+            if ($confVote->getConfId() === $this) {
+                $confVote->setConfId(null);
+            }
+        }
+
+        return $this;
+    }
+
+
 }
