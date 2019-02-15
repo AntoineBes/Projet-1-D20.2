14f9621ab6e3ee47c7de7b961e350c6a4dcc771a
lucasfoussier <lucasfoussier@gmail.com>
2019-02-15 15:34:25 +0100

    homepage

--- src/Repository/ConferenceRepository.php
+++ src/Repository/ConferenceRepository.php
@@ -12,39 +12,71 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  * @method Conference[]    findAll()
  * @method Conference[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class ConferenceRepository extends ServiceEntityRepository
-{
-    public function __construct(RegistryInterface $registry)
-    {
+class ConferenceRepository extends ServiceEntityRepository {
+
+    public function __construct(RegistryInterface $registry) {
         parent::__construct($registry, Conference::class);
     }
 
     // /**
     //  * @return Conference[] Returns an array of Conference objects
     //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('c.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
+
+    public function getAvaiableDates($page) {
+
+        $page--;
+        $page = $page * 2;
+
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = '
+        SELECT DATE_FORMAT(p.date, "%Y-%m-%d") as date
+        FROM conference p        
+        GROUP BY DATE_FORMAT(p.date, \'%Y-%m-%d\')
+        ORDER BY DATE_FORMAT(p.date, \'%Y-%m-%d\') DESC     
+        LIMIT 2
+        OFFSET              
+        ' . $page;
+
+
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        // returns an array of arrays (i.e. a raw data set)
+        return $stmt->fetchAll();
     }
-    */
+    
+    
+    public function getConferenceForADay($date) {
+        
+
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = '
+        SELECT *, DATE_FORMAT(p.date, \'%W %M %e %Y\') as btDate, DATE_FORMAT(p.date, \'%H:%i\') as timeDate 
+        FROM conference p        
+        WHERE DATE_FORMAT(p.date, \'%Y-%m-%d\') = \''.$date.'\'        
+        ';
+
+
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        // returns an array of arrays (i.e. a raw data set)
+        return $stmt->fetchAll();
+    }
+    
+    
 
     /*
-    public function findOneBySomeField($value): ?Conference
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
+      public function findOneBySomeField($value): ?Conference
+      {
+      return $this->createQueryBuilder('c')
+      ->andWhere('c.exampleField = :val')
+      ->setParameter('val', $value)
+      ->getQuery()
+      ->getOneOrNullResult()
+      ;
+      }
+     */
 }
